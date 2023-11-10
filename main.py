3ae736dbcc372cbcc0253ef9dbadc3b04e0d6d21
Miliano Fernandes de Oliveira Junior <69368192+MilianoJunior@users.noreply.github.com>
2023-11-10 14:24:22 -0300

    Criando os comandos git

---
+++ main.py
@@ -0,0 +1,28 @@
+
+comandos_git = '''
+
+Iniciar o Repositório Local: git init
+
+Verificar a Conexão: git remote -v
+
+Push (Envio): git push -u origin main
+
+Pull (Recebimento) : git pull origin main
+
+Atualizar (Sincronizar): git fetch origin
+
+Diferenças : git diff main origin/main
+
+Adicionar: git add .
+
+Criar um Commit : git commit -m "MENSAGEM_DE_COMMIT"
+
+'''
+
+'''
+Processo de adicionar mudanças:
+
+
+
+
+'''
