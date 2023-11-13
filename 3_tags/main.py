408569389b33af2ecdc42638c68f4242a831532d
Miliano Fernandes de Oliveira Junior <69368192+MilianoJunior@users.noreply.github.com>
2023-11-13 15:15:08 -0300

    update figma

--- 3_tags/main.py
+++ 3_tags/main.py
@@ -65,14 +65,66 @@ dict_modelo = {
     '10/11/2023 16:02': 36,
     '10/11/2023 16:03': 37,
     '10/11/2023 16:04': 38,
-    '10/11/2023 16:05': 39,
+    '10/11/2023 17:05': 0,
+    '11/11/2023 16:01': 39,
+    '11/11/2023 16:02': 40,
+    '11/11/2023 16:03': 41,
+    '11/11/2023 16:04': 42,
+    '11/11/2023 16:05': 49,
+    '25/11/2023 17:05': 89,
+    '25/11/2023 16:01': 90,
+    '25/11/2023 16:02': 91,
+    '25/11/2023 16:03': 92,
+    '25/11/2023 16:04': 93,
+    '25/11/2023 16:05': 0,
 }
 
-for data, valor in dict_modelo.items():
-    print(data, valor)
+# mostrar os dados do dict_modelo
+for key, value in dict_modelo.items():
+    print(key, value)
 
+# mostrar os valores do dict_modelo
+for value in dict_modelo.values():
+    print(value)
+
+# mostrar as chaves do dict_modelo
+for key in dict_modelo.keys():
+    print(key)
 
 # quantas bolitas foram acrescentadas hj para os dados do dict_modelo ?
+
+
+# Formula: A diferença entre o valor atual e o valor anterior
+import pandas as pd
+
+df = pd.DataFrame(dict_modelo.items(), columns=['data', 'valor'])
+
+# a última linha do dia atual
+# tem que ser diferente de zero
+valor1 = df['valor'].iloc[-1]
+cont = 0
+while valor1 == 0:
+    cont -= 1
+    valor1 = df['valor'].iloc[cont]
+    print(valor1)
+print(f'Esse é o último valor do dia atual: {valor1}')
+
+# a última linha do dia anterior
+# tem que ser diferente de zero
+data_atual = df['data'].iloc[-1].split(' ')[0]
+data_anterior = df['data'].iloc[-2].split(' ')[0]
+
+while data_atual == data_anterior:
+    cont -= 1
+    data_anterior = df['data'].iloc[cont].split(' ')[0]
+    print(data_atual, data_anterior, cont)
+
+
+
+
+
+
+
 
 # qual é formula para responde essa questão.
 
